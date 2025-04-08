3fe0ac080044da335b28c3e7f02e94010c307556
alexzatulin <alexorn1@gmail.com>
2025-04-08 14:16:45 +0300

    Git add commands

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 print("hello world")
-
+print("My name is Alex")
 #git add .
 #git commit -m “Commit message here"
 #git push origin master
 
-#echo "# 19_git_test" >> README.md
+#echo "# folder-name" >> README.md
 #git init
 #git add README.md
 #git commit -m "first commit"
 #git branch -M main
-#git remote add origin https://github.com/alexzatulin/19_git_test.git
+#git remote add origin https://github.com/alexzatulin/"folder-name".git
 #git push -u origin main
